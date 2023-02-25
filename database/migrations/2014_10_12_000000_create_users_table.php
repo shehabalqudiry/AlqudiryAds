fc3ff001cdc20a129520728aad2b76e9fdb8ecc9
شهاب القديري <shehabalqudiry@gmail.com>
2023-02-25 08:48:57 +0200

    setup frontend and tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('type')->default('user');
+            $table->string('photo')->default('user.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
